058a237a36eb28fa3cbb763ed21ff5d714f05c2c
futang yang <yangfutang@91guoxin.com>
2017-11-20 15:17:31 +0800

    添加pod

---
+++ MoblieProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoblieProject
+//
+//  Created by futang yang on 2017/11/20.
+//  Copyright © 2017年 futang yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
